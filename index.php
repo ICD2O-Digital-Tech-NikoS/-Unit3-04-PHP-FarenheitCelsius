897111e28cdf1cdee591d3b644d3a3359da15726
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-04-11 14:48:04 +0000

    u

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
     <meta name="keywords" content="immaculata, ics2o" />
     <meta name="author" content="Niko" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="apple-touch-icon" sizes="180x180" href="./faviconfolder/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="./faviconfolder/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="./faviconfolder/favicon-16x16.png" />
-    <link rel="manifest" href="./faviconfolder/site.webmanifest" />
+    <link rel="apple-touch-icon" sizes="180x180" href="./faviconf/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="./faviconf/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="./faviconf/favicon-16x16.png" />
+    <link rel="manifest" href="./faviconf/site.webmanifest" />
     <link rel="stylesheet" href="./css/style.css" />
     <title>Fahrenheit to Celsius</title>
   </head>
